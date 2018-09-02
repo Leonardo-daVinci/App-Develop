6b0cae2475579003acdcbeb93749c681063bf9e3
Leonardo <nocturnus.labs4@gmail.com>
2018-09-02 22:37:15 +0530

    message

--- notificationFunction/functions/index.js
+++ notificationFunction/functions/index.js
@@ -7,7 +7,6 @@ admin.initializeApp();
 exports.sendNotification = functions.database.ref('/notifications/{user_id}/{notification_id}').onWrite((change,context) => {
 
     const user_id = context.params.user_id;
-  
 
     console.log('User ID = ', user_id);
    // console.log('Notification ID = ', notification_id);
